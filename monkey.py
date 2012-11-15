8c2ff7b31a5c747ce31577595e42a24af556f751
nthorne <notrupertthorne@gmail.com>
2012-11-15 22:07:27 +0100

    reindented code

--- monkey.py
+++ monkey.py
@@ -26,93 +26,93 @@ from StringIO import StringIO
 
 
 def print_usage():
-  print "usage:", os.path.basename(sys.argv[0]), "HEADER FOOTER TEMPLATE CSV"
-  print
-  print "Translate CSV into output by, for each CSV line, replacing every"
-  print "occurence of %[0-9]+% within TEMPLATE with its respective CSV column."
-  print "HEADER and FOOTER is prepended and appended, respectively, to the output,"
-  print "which is written to stdout"
+    print "usage:", os.path.basename(sys.argv[0]), "HEADER FOOTER TEMPLATE CSV"
+    print
+    print "Translate CSV into output by, for each CSV line, replacing every"
+    print "occurence of %[0-9]+% within TEMPLATE with its respective CSV column."
+    print "HEADER and FOOTER is prepended and appended, respectively, to the output,"
+    print "which is written to stdout"
 
 
 class Parser:
-  def __init__(self, header, footer, template, csv):
-    self.__header = header
-    self.__footer = footer
-    self.__template = template
-    self.__csv = csv
+    def __init__(self, header, footer, template, csv):
+        self.__header = header
+        self.__footer = footer
+        self.__template = template
+        self.__csv = csv
 
-  def parse(self):
-    result = self.__header.read()
+    def parse(self):
+        result = self.__header.read()
 
-    for line in self.__csv:
-      fields = line.split(",")
-      fields = [l.strip() for l in fields]
-      fields = [item for item in fields if item]
+        for line in self.__csv:
+            fields = line.split(",")
+            fields = [l.strip() for l in fields]
+            fields = [item for item in fields if item]
 
-      if 0 != len(fields):
-        self.__template.seek(0)
+            if 0 != len(fields):
+                self.__template.seek(0)
 
-        for tmpl in self.__template:
-          matches = re.findall('%[0-9]+%', tmpl)
-          if matches:
-            for match in matches:
-              index = int(match.strip().strip('%'))
-              try:
-                tmpl = tmpl.replace(match.strip(), fields[index - 1])
-              except:
-                sys.stderr.write("Failed to access index %d for line %s, csv %s\n"
-                  %(index, tmpl, fields))
-          result += tmpl
+                for tmpl in self.__template:
+                    matches = re.findall('%[0-9]+%', tmpl)
+                    if matches:
+                        for match in matches:
+                            index = int(match.strip().strip('%'))
+                            try:
+                                tmpl = tmpl.replace(match.strip(), fields[index - 1])
+                            except:
+                                sys.stderr.write("Failed to access index %d for line %s, csv %s\n"
+                                                 %(index, tmpl, fields))
+                    result += tmpl
 
-    result += self.__footer.read()
+        result += self.__footer.read()
 
-    return result
+        return result
 
 
 if "__main__" == __name__:
-  if 5 != len(sys.argv) or '-h' == sys.argv[1] or '--help' == sys.argv[1]:
-    print_usage()
-    sys.exit(1)
+    if 5 != len(sys.argv) or '-h' == sys.argv[1] or '--help' == sys.argv[1]:
+        print_usage()
+        sys.exit(1)
 
-  try:
-    header = open(sys.argv[1])
-  except:
-    print "error: HEADER file", sys.argv[1], "not found."
-    print
-    print_usage()
-    sys.exit(1)
+    try:
+        header = open(sys.argv[1])
+    except:
+        print "error: HEADER file", sys.argv[1], "not found."
+        print
+        print_usage()
+        sys.exit(1)
 
-  try:
-    footer = open(sys.argv[2])
-  except:
-    print "error: FOOTER file", sys.argv[2], "not found."
-    print
-    print_usage()
-    sys.exit(1)
+    try:
+        footer = open(sys.argv[2])
+    except:
+        print "error: FOOTER file", sys.argv[2], "not found."
+        print
+        print_usage()
+        sys.exit(1)
 
-  try:
-    template = open(sys.argv[3])
-  except:
-    print "error: TEMPLATE file", sys.argv[3], "not found."
-    print
-    print_usage()
-    sys.exit(1)
+    try:
+        template = open(sys.argv[3])
+    except:
+        print "error: TEMPLATE file", sys.argv[3], "not found."
+        print
+        print_usage()
+        sys.exit(1)
 
-  try:
-    csv = open(sys.argv[4])
-  except:
-    print "error: CSV", sys.argv[4], "not found."
-    print
-    print_usage()
-    sys.exit(1)
+    try:
+        csv = open(sys.argv[4])
+    except:
+        print "error: CSV", sys.argv[4], "not found."
+        print
+        print_usage()
+        sys.exit(1)
 
-  p = Parser(header, footer, template, csv)
+    p = Parser(header, footer, template, csv)
 
-  out = p.parse();
+    out = p.parse();
 
-  header.close()
-  footer.close()
-  template.close()
-  csv.close()
+    header.close()
+    footer.close()
+    template.close()
+    csv.close()
 
-  print out
+    print out
